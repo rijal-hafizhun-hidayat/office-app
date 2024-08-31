cbcf81c000064580539253619f61d90937cf5143
unknown <rijal.1344@gmail.com>
2024-09-01 01:55:12 +0700

    feat: add admin middleware

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\OvertimeLetterController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -15,28 +16,32 @@ Route::middleware([AuthMiddleware::class])->group(function () {
     Route::prefix('/dashboard')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
     });
-    Route::prefix('/role')->group(function () {
-        Route::get('/', [RoleController::class, 'index'])->name('role.index');
-        Route::get('/create', [RoleController::class, 'create'])->name('role.create');
-        Route::post('/', [RoleController::class, 'store'])->name('role.store');
-        Route::get('/{id}', [RoleController::class, 'show'])->name('role.show');
-        Route::put('/{id}', [RoleController::class, 'update'])->name('role.update');
-        Route::delete('/{id}', [RoleController::class, 'destroy'])->name('role.destroy');
-    });
 
-    Route::prefix('/user')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('user.index');
-        Route::get('/create', [UserController::class, 'create'])->name('user.create');
-        Route::post('/', [UserController::class, 'store'])->name('user.store');
-        Route::get('/{id}', [UserController::class, 'show'])->name('user.show');
-        Route::put('/{id}', [UserController::class, 'update'])->name('user.update');
-        Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::middleware([AdminMiddleware::class])->group(function () {
+        Route::prefix('/role')->group(function () {
+            Route::get('/', [RoleController::class, 'index'])->name('role.index');
+            Route::get('/create', [RoleController::class, 'create'])->name('role.create');
+            Route::post('/', [RoleController::class, 'store'])->name('role.store');
+            Route::get('/{id}', [RoleController::class, 'show'])->name('role.show');
+            Route::put('/{id}', [RoleController::class, 'update'])->name('role.update');
+            Route::delete('/{id}', [RoleController::class, 'destroy'])->name('role.destroy');
+        });
 
-        Route::prefix('/{id}/change-password')->group(function () {
-            Route::get('/', [UserController::class, 'changePassword'])->name('user.change-password.index');
-            Route::patch('/', [UserController::class, 'updatePassword'])->name('user.change-password.patch');
+        Route::prefix('/user')->group(function () {
+            Route::get('/', [UserController::class, 'index'])->name('user.index');
+            Route::get('/create', [UserController::class, 'create'])->name('user.create');
+            Route::post('/', [UserController::class, 'store'])->name('user.store');
+            Route::get('/{id}', [UserController::class, 'show'])->name('user.show');
+            Route::put('/{id}', [UserController::class, 'update'])->name('user.update');
+            Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+            Route::prefix('/{id}/change-password')->group(function () {
+                Route::get('/', [UserController::class, 'changePassword'])->name('user.change-password.index');
+                Route::patch('/', [UserController::class, 'updatePassword'])->name('user.change-password.patch');
+            });
         });
     });
+
 
     Route::prefix('/overtime-letter')->group(function () {
         Route::get('/', [OvertimeLetterController::class, 'index'])->name('overtime-letter.index');
