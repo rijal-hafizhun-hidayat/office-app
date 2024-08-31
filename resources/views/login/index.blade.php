67b279455d79c31ceb5409b577b8a9cd1126412f
unknown <rijal.1344@gmail.com>
2024-08-31 09:28:53 +0700

    view: add login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,4 +1,31 @@
 @extends('app')
 @section('content')
-    <p>hello world</p>
+    <div class="d-flex align-items-center justify-content-center">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <form action="#" method="post">
+                        @csrf
+                        <div class="row mb-2">
+                            <div class="col">
+                                <label for="">Username</label>
+                                <input type="text" name="username" class="form-control" id="">
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control" id="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
