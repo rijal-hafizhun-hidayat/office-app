3aba92ce8367d25502d6dd570a62bade28114419
unknown <rijal.1344@gmail.com>
2024-08-31 15:56:37 +0700

    feat: add roles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login.index');
+});
+
+Route::prefix('/role')->group(function () {
+    Route::get('/', [RoleController::class, 'index'])->name('role.index');
+    Route::get('/create', [RoleController::class, 'create'])->name('role.create');
+    Route::post('/', [RoleController::class, 'store'])->name('role.store');
+    Route::get('/{id}', [RoleController::class, 'show'])->name('role.show');
+    Route::put('/{id}', [RoleController::class, 'update'])->name('role.update');
+    Route::delete('/{id}', [RoleController::class, 'destroy'])->name('role.destroy');
 });
